1984d01ed9ccdd6d0ac3236be1fcb06d4eba5df0
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2018-05-20 16:58:56 +0700

    Create view verification/new_request.blade.php

---
+++ resources/views/verification/new_request.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@push('scripts')
+    @inline('/js/utils/multi-image-preview.js')
+@endpush
+
+@push('styles')
+    @style('/css/multi-image-preview.css')
+@endpush
+
+@section('title', 'Buat Permintaan Baru untuk Pengecekkan')
+
+@section('content')
+    <form
+        name="new_request"
+        action="{{ route('new_request') }}"
+        method="post"
+        enctype="multipart/form-data">
+        <input id="sneakers_images" type="file" name="sneakers_images[]" multiple>
+
+        <h4>Pastikan semua sudut sneakers ada di semua gambar Anda</h4>
+        <div id="image-preview"></div>
+
+        <button type="submit">Kirim Permintaan</button>
+    </form>
+@endsection
+
