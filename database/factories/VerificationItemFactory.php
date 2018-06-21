e5ad9e48b151f4402ee177c2bf80197578e342be
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2018-06-21 16:40:11 +0700

    Create VerificationItemFactory

---
+++ database/factories/VerificationItemFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(App\VerificationItem::class, function (Faker $faker) {
+    $dateTime = $faker->dateTime();
+
+    return [
+        'status_review' => $faker->boolean,
+        'created_at' => $dateTime,
+        'updated_at' => $dateTime,
+    ];
+});
