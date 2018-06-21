50cf68dd1be2e702e8ab94b016d526e837f2a5bd
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2018-06-21 16:37:58 +0700

    Add relationship method in User model to VerificationItem model

--- app/User.php
+++ app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function verification_items()
+    {
+        return $this->hasMany('App\VerificationItem');
+    }
 }
