44a276ac27bc955c227825413f501bf8b6f5a45b
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2018-06-21 16:44:29 +0700

    Remove unnecessary code

--- database/seeds/VerificationItemsTableSeeder.php
+++ database/seeds/VerificationItemsTableSeeder.php
@@ -11,7 +11,6 @@ class VerificationItemsTableSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Faker\Factory::create();
         App\User::all()->each(function($user_model) {
             $rand_num = rand(0, 30);
             factory(App\VerificationItem::class, $rand_num)->make()->each(function($verification_item_model) use ($user_model) {
