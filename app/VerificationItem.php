096e5ee495f8c5da79b83f90e035bfcbc17c1720
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2018-06-21 16:36:14 +0700

    Create VerificationItem model

---
+++ app/VerificationItem.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class VerificationItem extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'status_review', 'created_at','updated_at',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [ ];
+}
