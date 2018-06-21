e43b1f050f5b2043f81cfcc788ce154631f0121c
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2018-06-21 16:40:53 +0700

    Create VerificationItemsTableSeeder

---
+++ database/seeds/VerificationItemsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class VerificationItemsTableSeeder extends Seeder
+{
+    /**
+     * Setiap pengguna memiliki jumlah verification item berbeda-beda
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+        App\User::all()->each(function($user_model) {
+            $rand_num = rand(0, 30);
+            factory(App\VerificationItem::class, $rand_num)->make()->each(function($verification_item_model) use ($user_model) {
+                $user_model->verification_items()->save($verification_item_model);
+            });
+        });
+    }
+}
