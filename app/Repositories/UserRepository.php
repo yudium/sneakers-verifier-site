f6a0061c050bb20d6f0f06d603957b7c3841d794
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2018-06-22 13:26:58 +0700

    Create UserRepository

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories;
+
+use Bosnadev\Repositories\Eloquent\Repository;
+
+class UserRepository extends Repository
+{
+    public function model()
+    {
+        return 'App\User';
+    }
+}
+
