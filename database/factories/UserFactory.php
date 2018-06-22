504de1e190389d856a1a7d9ccbe91aa1817b1376
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2018-06-22 13:24:21 +0700

    Add photo column to UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,5 +19,6 @@ $factory->define(App\User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => NULL,
+        'photo' => $faker->image('storage/app/public/user_photo_profile', 640, 480, null, false),
     ];
 });
