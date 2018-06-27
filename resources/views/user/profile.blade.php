53c5736ac50767467944e448ab0612bfa51d821f
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2018-06-27 10:32:30 +0700

    Show list of verification item in User Profile page

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -7,5 +7,12 @@
     <p>Tanggal bergabung: {{ $user->created_at }}</p>
     <p>Jumlah pengajuan: {{ $user->verification_items_count }}</p>
     <p>Jumlah yang belum direview: {{ $user->unreviewed_verification_items_count }}</p>
+
+    @foreach ($user->verification_items as $verification_item)
+    <div>
+        Tanggal upload: {{ $verification_item->created_at }}
+        Status review: {{ $verification_item->status_review }}
+    </div>
+    @endforeach
 @endsection
 
