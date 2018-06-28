2d65656bffb140e30905b41d8224cf75760365bb
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2018-06-28 16:04:29 +0700

    Add VerificationItemImageSeeder to DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsersTableSeeder::class,
             VerificationItemsTableSeeder::class,
+            VerificationItemImagesTableSeeder::class,
         ]);
     }
 }
