6855fdfa6f94487a9483b03dcf73fc6011f3d119
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2018-06-28 16:34:34 +0700

    Minor change profile.blade.php

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -3,16 +3,32 @@
 @section('title', 'Profil: '.$user->name)
 
 @section('content')
-    <img src="{{ asset('storage/user_photo_profile/ed6780c63ddfde598b0ca2ea33acea2f.jpg') }}" alt="Foto pengguna">
-    <p>Tanggal bergabung: {{ $user->created_at }}</p>
-    <p>Jumlah pengajuan: {{ $user->verification_items_count }}</p>
-    <p>Jumlah yang belum direview: {{ $user->unreviewed_verification_items_count }}</p>
-
-    @foreach ($user->verification_items as $verification_item)
-    <div>
-        Tanggal upload: {{ $verification_item->created_at }}
-        Status review: {{ $verification_item->status_review }}
+    <div id="left-container">
+        <img id="photo-profile" src="{{ asset('storage/user_photo_profile/'.$user->photo) }}" alt="Foto pengguna">
+        <div id="user-information">
+            <p>Tanggal bergabung: {{ $user->created_at }}</p>
+            <p>Jumlah pengajuan: {{ $user->verification_items_count }}</p>
+            <p>Jumlah yang belum direview: {{ $user->unreviewed_verification_items_count }}</p>
+        </div>
     </div>
-    @endforeach
+
+    <div id="main-container">
+        <!-- kotak daftar verification item -->
+        <div class="list-of-verification-item">
+            @foreach ($user->verification_items as $verification_item)
+                <!-- kotak item... setiap item berisi 3 gambard dan keterangan tanggal upload + status review -->
+                <div class="list-item">
+                    @foreach ($verification_item->verification_item_images as $verification_item_image)
+                        <img src="{{ asset('storage/verification_sneakers_images/'.$verification_item_image->path) }}" alt="Sneakers Image" width="150" height="120">
+                        @if ($loop->iteration == 3)
+                            @break
+                        @endif
+                    @endforeach
+                    Tanggal upload: {{ $verification_item->created_at }}
+                    Status review: {{ $verification_item->status_review }}
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
 
