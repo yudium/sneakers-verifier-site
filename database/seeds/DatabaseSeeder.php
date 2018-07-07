6cbe72538b08bf951bb59b8860a96d95601a0bc9
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2018-07-07 11:04:36 +0700

    Add VerificatorTableSeeder to DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             VerificationItemsTableSeeder::class,
             VerificationItemImagesTableSeeder::class,
             VerificationItemLinksTableSeeder::class,
+            VerificatorsTableSeeder::class,
         ]);
     }
 }
