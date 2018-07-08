69679282610b65c430f87647743c7280ef6cb4cf
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2018-07-08 12:46:39 +0700

    Create migration create_reviews_table

---
+++ database/migrations/2018_07_08_035828_create_reviews_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('verification_item_id');
+            $table->text('note')->nullable();
+            $table->char('status', 1);
+            $table->timestamps();
+            $table->foreign('verification_item_id')->references('id')->on('verification_items')
+                  ->onDelete('cascade')
+                  ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
