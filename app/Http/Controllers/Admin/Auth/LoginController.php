8f06412cbad15cd440c8444decc90a2258c7e3c9
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2018-07-22 08:08:09 +0700

    Create admin login page

---
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/admin/dashboard';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('admin_guest')->except('logout');
+    }
+
+    public function guard()
+    {
+        return Auth::guard('web_admin');
+    }
+
+    public function showLoginForm()
+    {
+        return view('admin.auth.login');
+    }
+}
---
+++ resources/views/admin/auth/login.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('title', 'Admin login')
+
+@section('content')
+<div class="login-container">
+    <div class="ctn-main-font ctn-min-color ctn-standar padding-20px">Admin Login</div>
+    <div class="lc-place">
+        <form action="{{ route('admin.login') }}" method="POST">
+            @csrf
+            <div class="lc-block">
+                <div for="email" class="ctn-main-font ctn-min-color ctn-14px ctn-thin">{{ __('E-Mail Address') }}</div>
+                <div>
+                    <input id="email" type="email" class="txt txt-primary-color form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                    @if ($errors->has('email'))
+                        <div class="ctn-main-font ctn-err-color ctn-14px ctn-thin">
+                            {{ $errors->first('email') }}
+                        </div>
+                    @endif
+                </div>
+            </div>
+            <div class="lc-block">
+                <div for="password" class="ctn-main-font ctn-min-color ctn-14px ctn-thin">{{ __('Password') }}</div>
+                <div>
+                    <input id="password" type="password" class="txt txt-primary-color form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                    @if ($errors->has('password'))
+                        <div class="ctn-main-font ctn-err-color ctn-14px ctn-thin">
+                            {{ $errors->first('password') }}
+                        </div>
+                    @endif
+                </div>
+            </div>
+            <div class="lc-block">
+                <div class="lc-right">
+                    <button type="submit" class="btn btn-main-color">
+                        {{ __('Login') }}
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
+
